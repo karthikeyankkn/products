e1cc7aac739b07f1704a4a633c7f77bdd2393be8
Karthikeyan Sundaraganesan <karthikeyan.sundaraganesan@tnqtech.com>
2024-09-22 16:06:44 +0530

    added isactive column datas

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -3,23 +3,23 @@ const db = require("../config/database");
 // ==> Método responsável por criar um novo 'Product':
 
 exports.createProduct = async (req, res) => {
-  const { productname, quantity, price } = req.body;
+  const { productname, quantity, price, isactive } = req.body;
   const { rows } = await db.query(
-    "INSERT INTO products (productname, quantity, price) VALUES ($1, $2, $3)",
-    [productname, quantity, price]
+    "INSERT INTO products (productname, quantity, price, isactive) VALUES ($1, $2, $3, $4)",
+    [productname, quantity, price, isactive]
   );
 
   res.status(201).send({
     message: "Product added successfully!",
     body: {
-      product: { productname, quantity, price }
+      product: { productname, quantity, price, isactive }
     },
   });
 };
 
 // ==> Método responsável por listar todos os 'Products':
 exports.listAllProducts = async (req, res) => {
-  const response = await db.query('SELECT * FROM products ORDER BY productid DESC');
+  const response = await db.query('SELECT * FROM products ORDER BY isActive DESC, productId DESC');
   res.status(200).send(response.rows);
 };
 
@@ -33,11 +33,11 @@ exports.findProductById = async (req, res) => {
 // ==> Método responsável por atualizar um 'Product' pelo 'Id':
 exports.updateProductById = async (req, res) => {
   const productId = parseInt(req.params.id);
-  const { productname, quantity, price } = req.body;
+  const { productname, quantity, price, isactive } = req.body;
 
   const response = await db.query(
-    "UPDATE products SET productname = $1, quantity = $2, price = $3 WHERE productId = $4",
-    [productname, quantity, price, productId]
+    "UPDATE products SET productname = $1, quantity = $2, price = $3, isactive = $4 WHERE productId = $5",
+    [productname, quantity, price, isactive, productId]
   );
 
   res.status(200).send({ message: "Product Updated Successfully!" });
